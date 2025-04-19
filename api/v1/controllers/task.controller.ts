315a61304b11304ee7a7274e23a8165c654a70dd
duylinhth7 <hoanglinhdz2k4@gmail.com>
2025-04-19 23:22:40 +0700

    update phần status, sort

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -3,9 +3,37 @@ import Tasks from "../models/tasks.model"
 
 //[GET] /api/v1/task
 export const index = async (req: Request, res: Response):Promise<void> => {
-    const task = await Tasks.find({
+    interface Find {
+        deleted: boolean,
+        status? : string,
+        title? : unknown
+    }
+    
+    let find: Find = {
         deleted: false
-    });
+    }
+
+    //status
+    if(req.query.status){
+        find.status = req.query.status.toString()
+    }
+    //end status
+
+    //find
+    // if(req.query.title){
+    //     find.title = req.query.title
+    // }
+    //end find
+
+    //Sort  
+    const sort: { [key: string]: any } = {};
+    if(req.query.sortKey && req.query.sortValue){
+        const sortKey = req.query.sortKey.toLocaleString();
+        sort[sortKey] = req.query.sortValue;
+    }
+    //end Sort
+    console.log(find)
+    const task = await Tasks.find(find).sort(sort);
     res.json(task)
 }
 
